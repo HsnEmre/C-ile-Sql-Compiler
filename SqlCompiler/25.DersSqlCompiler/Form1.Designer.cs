a5dcd6fd529956adfe72a9917e5625700a39267c
HsnEmre <HasanEmre.B@hotmail.com>
2022-07-15 00:26:44 +0300

    C# ile Sql Compiler
    istediğiniz bi veritabanı seçerek
    sql sorgularını program üzerinden çalıştırabilirsiniz

---
+++ SqlCompiler/25.DersSqlCompiler/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace _25.DersSqlCompiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CmbDb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbtablo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSorgu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(791, 452);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 493);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1253, 313);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(836, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(414, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Veri Bağlantısını Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CmbDb
+            // 
+            this.CmbDb.FormattingEnabled = true;
+            this.CmbDb.Location = new System.Drawing.Point(852, 241);
+            this.CmbDb.Name = "CmbDb";
+            this.CmbDb.Size = new System.Drawing.Size(425, 29);
+            this.CmbDb.TabIndex = 3;
+            this.CmbDb.SelectedIndexChanged += new System.EventHandler(this.CmbDb_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(836, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Veri Tabanları:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(840, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(410, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "DbBaglantiDeğiştir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbtablo
+            // 
+            this.cmbtablo.FormattingEnabled = true;
+            this.cmbtablo.Location = new System.Drawing.Point(852, 311);
+            this.cmbtablo.Name = "cmbtablo";
+            this.cmbtablo.Size = new System.Drawing.Size(425, 29);
+            this.cmbtablo.TabIndex = 3;
+            this.cmbtablo.SelectedIndexChanged += new System.EventHandler(this.CmbDb_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(836, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tablolar:";
+            // 
+            // btnSorgu
+            // 
+            this.btnSorgu.Location = new System.Drawing.Point(852, 387);
+            this.btnSorgu.Name = "btnSorgu";
+            this.btnSorgu.Size = new System.Drawing.Size(411, 67);
+            this.btnSorgu.TabIndex = 6;
+            this.btnSorgu.Text = "button3";
+            this.btnSorgu.UseVisualStyleBackColor = true;
+            this.btnSorgu.Click += new System.EventHandler(this.btnSorgu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1289, 818);
+            this.Controls.Add(this.btnSorgu);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbtablo);
+            this.Controls.Add(this.CmbDb);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox CmbDb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbtablo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSorgu;
+    }
+}
+
---
+++ SqlCompiler/25.DersSqlCompiler/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _25.DersSqlCompiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        void DBBaglantisi()
+        {
+            baglanti = new SqlConnection(@"Data Source=DESKTOP-972PD8E\SQLEXPRESS;Initial Catalog=Master;Integrated Security=True");
+            baglanti.Open();
+            komut = new SqlCommand("select name from sys.databases", baglanti);
+            dr = komut.ExecuteReader();
+
+            while (dr.Read())
+            {
+                CmbDb.Items.Add(dr[0].ToString());
+            }
+            baglanti.Close();
+        }
+        void DBDeğiştir()
+        {
+            baglanti = new SqlConnection(@"Data Source=DESKTOP-972PD8E\SQLEXPRESS;Initial Catalog="+CmbDb.Text+";Integrated Security=True");
+            baglanti.Open();
+            komut = new SqlCommand("select name from sys.databases", baglanti);
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                CmbDb.Items.Add(dr[0].ToString());
+            }
+            baglanti.Close();
+        }
+        void TabloListele()
+        {
+            baglanti = new SqlConnection(@"Data Source=DESKTOP-972PD8E\SQLEXPRESS;Initial Catalog=" + CmbDb.Text + ";Integrated Security=True");
+            baglanti.Open();
+            komut = new SqlCommand("select name from sys.Tables", baglanti);
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbtablo.Items.Add(dr[0].ToString());
+            }
+            baglanti.Close();
+        }
+        void sorguYaz()
+        {
+            baglanti = new SqlConnection(@"Data Source=DESKTOP-972PD8E\SQLEXPRESS;Initial Catalog=" + CmbDb.Text + ";Integrated Security=True");
+            baglanti.Open();
+            komut = new SqlCommand(richTextBox1.Text, baglanti);
+            komut.ExecuteNonQuery();
+            string sorgu = richTextBox1.Text;
+            da = new SqlDataAdapter(sorgu, baglanti);
+            dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            baglanti.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DBBaglantisi();
+            MessageBox.Show("db açıldı");
+        }
+
+        private void CmbDb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DBDeğiştir();
+            TabloListele();
+            MessageBox.Show("db değişti ve tablolar Yüklendi");
+            
+        }
+
+        private void btnSorgu_Click(object sender, EventArgs e)
+        {
+            sorguYaz();
+        }
+    }
+}
